82698fb779c9a8a9204a4e8ca01e81654a2ef2dc
pmain8 <pmain8@vt.edu>
2014-07-08 13:33:03 -0400

    Added user model with devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:email => "asdf@jkl.com", :password => "asdfasdf", :password_confirmation => "asdfasdf").save!
